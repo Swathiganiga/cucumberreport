4a12a6f075e6271765bf9cde8c4a73b0dbebbb44
swathi <swathi@GISI-0NVH5P.gisi.com>
2019-07-08 22:14:52 +0530

    2nd

--- cucumber_demo/src/test/java/com/swathi/demo/testrunner.java
+++ cucumber_demo/src/test/java/com/swathi/demo/testrunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="classpath:login\\Logintest.feature",glue="com.swathi.demo",plugin= {"html:target/test-report","junit:target/junit-xml-report.xml","json:target/json-report.json"})
+@CucumberOptions(features="classpath:login\\Logintest.feature",glue="com.swathi.demo",plugin= {"html:target/test-report","html:target/cucum.html","junit:target/junit-xml-report.xml","json:target/json-report.json"})
 public class testrunner {
 
 }
